fe0669d451005bac217116a7e6294fbca58d9e91
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-17 15:13:26 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import Bottoni from './button/button'
+
+function App() {
+  <Bottoni/>
+}
+
+export default App
---
+++ src/button/button.jsx
@@ -0,0 +1,14 @@
+export default function Bottoni() {
+    {
+        return (
+            <>
+            <button type="button" class="btn btn-primary">HTML</button>
+            <button type="button" class="btn btn-primary">CSS</button>
+            <button type="button" class="btn btn-primary">JavaScript</button>
+            <button type="button" class="btn btn-primary">Node.js</button>
+            <button type="button" class="btn btn-primary">Express</button>
+            <button type="button" class="btn btn-primary">ReactJs</button>
+            </>
+        )
+    }
+}
